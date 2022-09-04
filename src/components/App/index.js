2ea9f90379b4f26c5c6a6dcf95677b8f3ab6400e
Lucas <baiba.lucas@gmail.com>
2022-09-04 18:38:02 +0200

    remove last fix about mobile screen size

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,14 +8,6 @@ import Footer from '../Footer';
 import Header from '../Header';
 import Recruitment from '../Recruitment';
 
-const appHeight = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-};
-
-window.addEventListener('resize', appHeight);
-appHeight();
-
 const App = () => {
   return (
     <div className='app'>
